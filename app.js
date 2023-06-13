253394deaaf9b0d123ea3f39a08f2f10586f3161
andrés <olivera.h.andres@gmail.com>
2023-06-13 12:41:41 -0300

    agrega estructura inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 3030
+
+// Rutas
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')))
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname,'views','contact.html')))
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
+app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')))
+
+app.listen(PORT, () => console.log('servidor corriendo en http://localhost:' + PORT))
